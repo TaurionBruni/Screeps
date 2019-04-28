ac19d333fb621f00f43974866cf222732c61af27
Aaron Gudrian <aagudria@utica.edu>
2019-04-27 20:12:38 -0400

    added STRUCTURE_CONTAINER

--- Master/role.harvester.js
+++ Master/role.harvester.js
@@ -21,8 +21,7 @@ const roleBuilder = {
                 //todo add to spawn
                 const targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-                        return (structure.structureType === STRUCTURE_EXTENSION || structure.structureType === STRUCTURE_SPAWN) &&
-                            structure.energy < structure.energyCapacity;
+                        return (structure.structureType === STRUCTURE_CONTAINER || structure.structureType === STRUCTURE_SPAWN || structure.structureType === STRUCTURE_EXTENSION) && structure.energy < structure.energyCapacity;
                     }
                 });
                 if (targets.length > 0) {
